0b795ec28b45e364e5fe8f16157063830e396cb6
iram <juaniramgamezaguilar@gmail.com>
2025-07-08 17:57:44 -0600

    all porject

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,9 @@ import { AboutComponent } from './components/about/about.component';
 import { SoftwareComponent } from './components/software/software.component';
 
 export const routes: Routes = [
-    {path: 'certifications',component: CertificationsComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'software', component: SoftwareComponent},
+    { path: '', redirectTo: '/about', pathMatch: 'full' }, // Ruta por defecto
+    { path: 'certifications', component: CertificationsComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'software', component: SoftwareComponent },
+    { path: '**', redirectTo: '/about' } // Redirige cualquier ruta no reconocida
 ];
